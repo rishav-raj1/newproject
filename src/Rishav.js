c8a4be5572db552e01a1a2f8f7f6a1bf46d8a70c
rishav-raj1 <rishavkumarraj801@gmail.com>
2021-06-21 17:42:14 +0530

    Added UseState in Material UI

--- src/Rishav.js
+++ src/Rishav.js
@@ -1,3 +1,189 @@
+{/*import React, { useState } from 'react'
+import { Typography, Divider, Toolbar, IconButton, Badge, makeStyles, Button, Grid } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import HomeIcon from '@material-ui/icons/Home';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { Container } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    title: {
+        flexGrow: 1,
+    },
+    realtext: {
+      color: 'black',
+      marginTop: 190,   
+  },
+
+    btn: {
+      marginTop: 20,
+  }
+
+
+}));
+
+const Rishav = () => {
+
+    const classes=useStyles();
+
+    // UseState text //    
+
+    const [CurrentValue, UpdatedValue] = useState('Welcome to Blue Berry e-Services Pvt. Ltd.');
+
+    const FunCall = () => {
+    UpdatedValue('Network Support & Maintenance IT Support And Cloud Services & Data Management ');
+    }
+
+    return (
+        <div>
+          <Toolbar >
+          <IconButton color="inherit">
+              <MenuIcon />
+          </IconButton>
+            <Typography className={classes.title}>
+            Blue Berry
+          </Typography>
+          <IconButton color="inherit">
+              <HomeIcon />
+          </IconButton>
+          <IconButton color="inherit">
+              <AccountCircleIcon />
+          </IconButton>
+          <IconButton color="inherit">
+          <Badge badgeContent={4} color="primary">
+          <NotificationsIcon />
+          </Badge>
+          </IconButton>
+          </Toolbar>
+          <Divider />
+
+
+          <Container align="center">
+
+         <Typography  className={classes.realtext}
+             variant="h2">
+              {CurrentValue}
+          <Button className={classes.btn}
+             variant="contained" 
+             size="large" 
+             color= "primary"
+              onClick = {() => {FunCall()}}>
+               Our Services
+          </Button>
+         </Typography>
+
+
+</Container>
+          
+     
+         
+        </div>
+    )
+}
+
+export default Rishav
+*/}
+
+
+
+///// header  //////////
+{/*
+
+import React from 'react'
+import { Typography, Divider, Toolbar, IconButton, Badge, makeStyles, Button, Grid } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import HomeIcon from '@material-ui/icons/Home';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+const useStyles = makeStyles(() => ({
+    title: {
+        flexGrow: 1,
+    },
+    tagline: { 
+      fontFamily: "Montserrat",
+      marginTop: 150,
+    },
+
+
+}));
+
+const Header = () => {
+
+    const classes=useStyles();
+
+    return (
+        <div>
+          <Toolbar >
+          <IconButton color="inherit">
+              <MenuIcon />
+          </IconButton>
+            <Typography className={classes.title}>
+            Blue Berry
+          </Typography>
+          <IconButton color="inherit">
+              <HomeIcon />
+          </IconButton>
+          <IconButton color="inherit">
+              <AccountCircleIcon />
+          </IconButton>
+          <IconButton color="inherit">
+          <Badge badgeContent={4} color="primary">
+          <NotificationsIcon />
+          </Badge>
+          </IconButton>
+          </Toolbar>
+          <Divider />
+
+            <Typography className={classes.tagline} >
+            <Typography variant="h3" align="center" color="inherit" gutterBottom>
+             Welcome to Blue Berry e-Services Pvt. Ltd.
+            </Typography>
+
+            <Typography variant="h6" align="center" color="textPrimary" paragraph>
+              We offer services and solutions that empower our customers to embrace technology to grow. 
+            </Typography>
+            </Typography>
+            
+
+            <Grid container direction="column" alignItems="center" justify="center">
+
+            <Button variant="contained" size="large" color="primary">
+              Our Services
+            </Button>
+
+            </Grid>
+          
+            
+
+            
+            
+          
+
+
+
+         
+        </div>
+    )
+}
+
+export default Header
+*/}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 {/*import React from 'react'
 import { Typography, AppBar, Button, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
 import { PhotoCamera } from '@material-ui/icons'
@@ -52,3 +238,5 @@ const Rishav = () => {
 }
 
 export default Rishav*/}
+
+
